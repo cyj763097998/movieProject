2b79355ed059637363d8ab4bd776d80c3a68b3bc
Your Name <you@example.com>
2018-12-12 17:10:47 +0800

    2222

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,9 @@
 #coding:utf8
 from flask import  Flask,render_template
 from flask_sqlalchemy import SQLAlchemy
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
 app =  Flask(__name__)
 app.debug = True
